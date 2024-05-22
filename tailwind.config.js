2d9526414820a865c87cc82174a8233b99051d74
11322brar <11322brar@gmail.com>
2024-05-22 22:16:51 +0530

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      'theme-blue': '#1364F1',
+      'theme-dark-blue': '#0648EF',
+      'links-blue': '#2B84EA',
+      'nav-grey': '#5F6C7D',
+      'gradient-start': '#FFFFFF',
+      'gradient-mid': '#F1F5FE',
+      'gradient-end': '#FEFEFE'
+    },
+    extend: {
+      fontFamily: {
+        'lato': ['Lato']
+      },
+    },
+    plugins: [],
+  }
 }
-
